78dda9b1b1fea8b73ffe4d9e2d03a1d460a9f920
Philip Sterling <philipssterlings@gmail.com>
2019-06-04 20:32:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,10 @@
 class Dog 
-  def initialize(name, breed: "Mutt") 
+  def initialize(name, breed) 
     @breed = breed
+    @name = name
+    end
+    def initialize(name)
+    @breed = "Mutt"
     @name = name
     end
   end
